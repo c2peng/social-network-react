9e053a8de18635f4f34c6f76b97522b8febee37f
Cheng <c2peng@ucsd.edu>
2020-10-21 13:25:09 -0700

    reformat code

--- src/pages/home.js
+++ src/pages/home.js
@@ -5,40 +5,40 @@ import axios from "axios";
 import Scream from "../components/Scream";
 
 export class home extends Component {
-	state = {
-		screams: null,
-	};
-	componentDidMount() {
-		axios
-			.get("/screams")
-			.then((result) => {
-				this.setState({
-					screams: result.data,
-				});
-			})
-			.catch((err) => {
-				console.error(err);
-			});
-	}
-	render() {
-		let recentScreamsMarkup = this.state.screams ? (
-			this.state.screams.map((scream) => {
-				return <Scream key={scream.screamId} scream={scream} />;
-			})
-		) : (
-			<p>Loading...</p>
-		);
-		return (
-			<Grid container spacing={3}>
-				<Grid item sm={8} xs={12}>
-					{recentScreamsMarkup}
-				</Grid>
-				<Grid item sm={4} xs={12}>
-					Profile
-				</Grid>
-			</Grid>
-		);
-	}
+  state = {
+    screams: null,
+  };
+  componentDidMount() {
+    axios
+      .get("/screams")
+      .then((result) => {
+        this.setState({
+          screams: result.data,
+        });
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }
+  render() {
+    let recentScreamsMarkup = this.state.screams ? (
+      this.state.screams.map((scream) => {
+        return <Scream key={scream.screamId} scream={scream} />;
+      })
+    ) : (
+      <p>Loading...</p>
+    );
+    return (
+      <Grid container spacing={3}>
+        <Grid item sm={8} xs={12}>
+          {recentScreamsMarkup}
+        </Grid>
+        <Grid item sm={4} xs={12}>
+          Profile
+        </Grid>
+      </Grid>
+    );
+  }
 }
 
 export default home;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,4 @@
-import {createStore, combineReducers, applyMiddleware, compose} from "redux"
+import { createStore, combineReducers, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 
 import userReducer from "./reducers/userReducer";
@@ -10,13 +10,18 @@ const initialState = {};
 const middleware = [thunk];
 
 const reducers = combineReducers({
-    user: userReducer,
-    data: dataReducer,
-    ui: uiReducer
+  user: userReducer,
+  data: dataReducer,
+  ui: uiReducer,
 });
 
-const store = createStore(reducers, initialState, compose(applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
+const store = createStore(
+  reducers,
+  initialState,
+  compose(
+    applyMiddleware(...middleware),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+);
 
 export default store;
-
